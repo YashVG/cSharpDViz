86ae1f3366385366fa1474d6d4aa3ef8d8db022d
Yash <garg.yashvardhan@gmail.com>
2024-05-02 08:02:15 +0400

    removed unecessary class

--- Program.cs
+++ Program.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CSharpDViz
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("##############################");
-            Console.WriteLine("WELCOME TO THE C# DATA VISUALIZER");
-            Console.WriteLine("##############################");
-            List<Number> numbers = new List<Number>();
 
-            while (numbers.Count < 3)
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("##############################");
+        Console.WriteLine("WELCOME TO THE C# DATA VISUALIZER");
+        Console.WriteLine("##############################");
+        Console.WriteLine();
+
+        List<int> numbers = new List<int>();
+        bool continueListBuild = true;
+
+        while (continueListBuild)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            try
             {
-                Console.Write("Enter a number: ");
-                string input = Console.ReadLine();
-                try
-                {
-                    int value = int.Parse(input);  // Convert string to int
-                    numbers.Add(new Number(value));  // Create a new Number instance and add it to the list
-                    Console.WriteLine("Added new number!");
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid input! Please enter a valid number.");
-                }
+                int value = int.Parse(input);  // Convert string to int
+                numbers.Add(value);  // Create a new Number instance and add it to the list
+                Console.WriteLine("Added new number!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+
+            Console.Write("Do you want to add more numbers [y/n]: ");
+            input = Console.ReadLine();
+            if (input.Equals("n"))
+            {
+                continueListBuild = false;
             }
         }
+
+        Console.WriteLine();
+        Console.Write("Here is your list of numbers: ");
+        Console.WriteLine(string.Join(", ", numbers));
     }
 }
--- obj/Debug/net8.0/cSharpDViz.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpDViz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpDViz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b748c764669baeadc1cbbf9439a231c77fee92d")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpDViz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpDViz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
